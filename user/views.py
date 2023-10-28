55588d17ac13e7ba66b08716f2bbfba97b5b9c4a
kevalrajpalknight <kevalprogram100@gmail.com>
2023-10-28 15:21:58 +0530

    modify the search and ordering filter

--- user/views.py
+++ user/views.py
@@ -1,14 +1,39 @@
 from django.contrib.auth import get_user_model
+from django.db.models import Q
+from functools import reduce
 from rest_framework import generics, filters
 
 from .serializers import UserSerializer
 
 
 class UserListView(generics.ListCreateAPIView):
-    queryset = (
-        get_user_model()
-        .objects.select_related("address")
-        .prefetch_related("certifications", "profession")
-        .all().order_by('name')
-    )
     serializer_class = UserSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = [
+        "name",
+        "email",
+        "phone",
+        "address__local_address",
+        "address__city",
+        "certifications__certificate_name",
+        "profession__profession",
+    ]
+    ordering_fields = [
+        "name",
+        "email",
+        "phone",
+        "address__local_address",
+        "address__city",
+        "certifications__certificate_name",
+        "profession__profession",
+    ]
+
+    def get_queryset(self):
+        queryset = (
+            get_user_model()
+            .objects.select_related("address")
+            .prefetch_related("certifications", "profession")
+            .all()
+            .order_by("name")
+        )
+        return queryset
